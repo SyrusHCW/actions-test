40aa0283fca9275721260927189d3f4fa6fe6fee
SyrusHCW <syrushcw@gmail.com>
2024-05-13 10:37:23 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,16 @@
-import logging
-import logging.handlers
+import boto3
 import os
 
-import requests
-
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-logger_file_handler = logging.handlers.RotatingFileHandler(
-    "status.log",
-    maxBytes=1024 * 1024,
-    backupCount=1,
-    encoding="utf8",
-)
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-logger_file_handler.setFormatter(formatter)
-logger.addHandler(logger_file_handler)
-
-try:
-    SOME_SECRET = os.environ["SOME_SECRET"]
-except KeyError:
-    SOME_SECRET = "Token not available!"
-    #logger.info("Token not available!")
-    #raise
 
 
-if __name__ == "__main__":
-    logger.info(f"Token value: {SOME_SECRET}")
+if __name__ == '__main__':
+    client = boto3.client('route53', 
+        aws_access_key_id=os.environ["KEY"], 
+        aws_secret_access_key=os.environ["SECRET"], 
+        region_name='us-east-1')
 
-    r = requests.get('https://weather.talkpython.fm/api/weather/?city=Berlin&country=DE')
-    if r.status_code == 200:
-        data = r.json()
-        temperature = data["forecast"]["temp"]
-        logger.info(f'Weather in Berlin: {temperature}')
+
+    response = client.create_hosted_zone(
+        Name='oggilliam.net',
+        CallerReference='string'
+    )
